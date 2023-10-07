fd09f45bda2e7a0f6f46ad705aca6c35844485b5
Himanshu <92928990+himanshu060301@users.noreply.github.com>
2023-10-07 11:49:46 +0530

    Update Create.js

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -32,7 +32,7 @@ const Create = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch("http://localhost:8080/post", {
+    fetch("https://jobpost-backend.onrender.com/post", {
       method: "POST", // or 'PUT'
       headers: {
         "Content-Type": "application/json",
